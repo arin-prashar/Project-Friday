1d0dc7b28d0250529940be93f4e7464bce8d4c17
arin-prashar <prashararin@gmail.com>
2023-05-21 20:45:22 +0530

    output correction
    Signed-off-by: arin-prashar <prashararin@gmail.com>

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def Friday():
                 tsk = question[3:]
                 task(tsk)
             elif question.startswith("tell"):
-                res = client.get_answer(question)
+                res = client.get_answer(question)['content']
                 audio.speak(res)
             
 def task(tsk):
